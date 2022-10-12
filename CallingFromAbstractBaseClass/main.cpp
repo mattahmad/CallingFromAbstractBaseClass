a17462d9676c26a8d36e6877bd40f3a3f0868375
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-11 22:06:39 -0400

    Calling from Abstract Base Class Code Review

--- CallingFromAbstractBaseClass/main.cpp
+++ CallingFromAbstractBaseClass/main.cpp
@@ -1,14 +1,38 @@
-//
-//  main.cpp
-//  CallingFromAbstractBaseClass
-//
-//  Created by Matt Ahmad on 2022-10-11.
-//
-
 #include <iostream>
+using namespace std;
+
+class Polygon {
+protected:
+    int width, height;
+public:
+    void set_values (int a, int b)
+        { width=a; height=b; }
+    virtual int area() = 0;
+    void printarea()
+    { cout << this->area() << '\n'; }
+};
+
+class Rectangle: public Polygon {
+    public:
+        int area (void)
+    { return (width * height); }
+};
+
+class Triangle: public Polygon {
+    public:
+        int area (void)
+    { return (width * height / 2); }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    Rectangle rect;
+    Triangle trgl;
+    Polygon * ppoly1 = &rect;
+    Polygon * ppoly2 = &trgl;
+    ppoly1->set_values (4,5);
+    ppoly2->set_values (4,5);
+    ppoly1->printarea();
+    ppoly2->printarea();
     return 0;
 }
